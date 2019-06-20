401ed0960d04e232a7541d90f65a3856633aa7d1
julianrees11 <julianrees11@yahoo.co.uk>
2019-06-20 16:35:45 -0400

    First Commit

---
+++ src/Start_Controller.java
@@ -0,0 +1,27 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Start_Controller implements Initializable {
+    @FXML
+    private Label labelTitle;
+
+    @FXML
+    private Button buttonStart;
+
+    @FXML
+    private Button buttonControls;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    public void start(){
+        
+    }
+}
+
